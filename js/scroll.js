dd70f9d52125f2c45c0b1a5e02b9ce5e7724b639
Aleksei <alekseigalasov@gmail.com>
2019-11-30 21:35:25 +0200

    Header done

--- js/scroll.js
+++ js/scroll.js
@@ -3,8 +3,21 @@ const progress = document.querySelector('.progress-bar__progress');
 window.addEventListener('scroll', progressBar);
 
 function progressBar(event) {
-    let windowScroll = document.documentElement.scrollTop;
+    let windowScroll = document.documentElement.scrollTop || document.body.scrollTop; 
     let windowHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight
     let per = windowScroll / windowHeight * 100;
     progress.style.width = per + '%';
 }
+
+
+
+const btn = document.querySelector('.cirlce');
+const close = document.querySelector('.overlay__close');
+
+btn.addEventListener('click', ()=> {
+    document.querySelector('.overlay').style.height = '100%';
+})
+
+close.addEventListener('click', ()=> {
+    document.querySelector('.overlay').style.height = '0';
+})
